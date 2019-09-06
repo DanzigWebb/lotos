31535cf82cdba4b53cebe5d8166fdbec1070f062
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-09-06 13:27:12 +0300

    mob

--- js/common.js
+++ js/common.js
@@ -4,30 +4,39 @@ let scroll = {
   btnDawn: document.querySelector('.section-bottom__btn'),
 
   menu: document.querySelector('#menu'),
+  menuLinks: document.querySelectorAll('.menu__item'),
   sectBottom: document.querySelector('.section-bottom'),
 
   count: 1,
 
 
   start() {
-    this.menuNaming()
-    this.sectRightContent();
+    this.menuNaming();
+    this.sectNumberContent();
     this.btnDawn.addEventListener('click', () => {
       this.scrollDown()
     })
+    this.menuControl()
   },
   scrollDown() {
     this.sectWrap.style.transform = `translateY(-${this.count * this.step}px)`;
     this.count ++;
     this.changeDinamicContent()
   },
+  menuControl() {
+    this.menuLinks.forEach((link, i) => {
+      link.addEventListener('click', ()=> {
+        this.count = i;
+        this.scrollDown()
+      })
+    })
+  },
 
   changeDinamicContent() {
     this.fadeFixedEl();
-    this.sectRightContent();
+    this.sectNumberContent();
     this.menuNaming();
   },
-
   fadeFixedEl() { // Скрытие фиксированных блоков при пролистывании слайда
     this.menu.classList.add('fixed-hide');
     this.sectBottom.classList.add('fixed-hide');
@@ -36,17 +45,15 @@ let scroll = {
       this.sectBottom.classList.remove('fixed-hide');
     }, 600);
   },
-  sectRightContent() { // Нумеровка слайдов
-    let activeSlide = document.querySelector('.section-right__active');
-    let allSlide = document.querySelector('.section-right__all');
+  sectNumberContent() { // Нумеровка слайдов
+    let activeSlide = document.querySelector('.section-number__active');
+    let allSlide = document.querySelector('.section-number__all');
     activeSlide.innerHTML = ("0"+this.count).slice(-2);
     allSlide.innerHTML = ("- 0"+this.sectWrap.children.length);
   },
-
-  
   menuNaming() { // Именование слайдов
     let content = { 
-      menu: ['Добро пожаловать', 'Направления и классы', 'Пробное занятие', 'Преподаватели', 'Абонементы', 'Расписание', 'Отзывы', 'Пробное занятие', 'Контакты', 'Вернуться'],
+      menu: ['Добро пожаловать', 'Направления и классы', 'Пробное занятие', 'Преподаватели', 'Абонементы', 'Расписание', 'Отзывы', 'Акции', 'Контакты', 'Вернуться'],
     };
     let el = this.menu.querySelector('.menu-sectname');
     el.innerHTML = content.menu[this.count - 1];
